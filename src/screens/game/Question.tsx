db20fdd25cf77c4afc3dbe3b0eedd0141631b574
Ben Crowl <mrcrowl@gmail.com>
2022-11-05 13:38:57 +1300

    Fixed scroll.

--- src/screens/game/Question.tsx
+++ src/screens/game/Question.tsx
@@ -3,12 +3,14 @@ import { css } from '@emotion/react';
 const QUESTION_STYLES = css`
   font-size: 17vh;
   width: 100vw;
-  height: 80vw;
+  height: calc(100vh - 100vw - 3rem); 
   justify-content: center;
   align-items: center;
   display: flex;
   padding-bottom: 0.8rem;
   font-weight: 100;
+  position: absolute;
+  top: 0;
 `;
 
 type QuestionProps = { prompt: string };
