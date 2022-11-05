93c528c886abfbb05ae27b9fefff2531736b8115
Ben Crowl <mrcrowl@gmail.com>
2022-11-06 06:59:38 +1300

    Update build script.

--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,10 @@ echo 'Building Vite assets'
 npm run typecheck
 npm run build:vite
 rm -f dist/mockServiceWorker.js
+cp dist/index.html dist/404.html
 cp -r dist/* ../basicfacts.github.io
+cd ../basicfacts.github.io
+git add .
+git commit -m "$(date '+%s')"
+git push
+cd ../basicfacts
